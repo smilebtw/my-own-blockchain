6ed3e2c35c9681ab193f582d096ec5ca5200cd04
sudoga <cloudgabriel@protonmail.com>
2022-06-12 20:07:56 -0300

    chore: create base methods

---
+++ src/main.py
@@ -0,0 +1,23 @@
+class Blockchain():
+    def __init__():
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new Block into the chain
+        pass
+
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last block in the chain
+        pass
+
