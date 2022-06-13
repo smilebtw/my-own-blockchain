44cf8624df8eaa1a07ebb3f77c582656b8ddbac7
sudoga <cloudgabriel@protonmail.com>
2022-06-12 21:43:36 -0300

    refactor: organize directories

--- src/blockchain.py
+++ src/blockchain.py
@@ -4,7 +4,6 @@ import hashlib
 from uuid import uuid4
 from textwrap import dedent
 
-from flask import Flask, jsonify, request
 
 class Blockchain():
 
@@ -90,57 +89,3 @@ class Blockchain():
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
-app = Flask(__name__)
-
-node_identifier = str(uuid4()).replace('-', '')
-
-blockchain = Blockchain()
-
-@app.route("/mine", methods=['GET'])
-def mine():
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
-
-    # Premio por minerar = 1
-
-    blockchain.new_transaction(sender="0", recipient=node_identifier, amount=1)
-
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
-
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
-
-@app.route("/transactions/new", methods=['POST'])
-def new_transaction():
-    values = request.get_json()
-
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
-    
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-
-    response = {
-        'message': f'Transaction will be added to Block {index}'
-    }
-    
-    return jsonify(response), 201
-
-@app.route("/chain", methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
