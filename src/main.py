747d36748163b29e5d9bda7f5e2f2ecbaf4bf832
sudoga <cloudgabriel@protonmail.com>
2022-06-12 21:15:31 -0300

    chore: implement mining route

--- src/main.py
+++ src/main.py
@@ -98,11 +98,41 @@ blockchain = Blockchain()
 
 @app.route("/mine", methods=['GET'])
 def mine():
-    return "Mining a block"
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # Premio por minerar = 1
+
+    blockchain.new_transaction(sender="0", recipient=node_identifier, amount=1)
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 @app.route("/transactions/new", methods=['POST'])
 def new_transaction():
-    return "New transaction"
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {
+        'message': f'Transaction will be added to Block {index}'
+    }
+    
+    return jsonify(response), 201
 
 @app.route("/chain", methods=['GET'])
 def full_chain():
